c9e5c49eb3d62fb900292a3b9fb09e69be4ea52b
Arthur Cesarino <contatocesarino@gmail.com>
2024-09-14 17:07:18 +0200

    updates

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,2 +1,8 @@
 require("arthur.core")
 require("arthur.lazy")
+
+-- Add this in your init.lua or core config file
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = { "*.tf", "*.hcl" },
+	command = "set filetype=terraform",
+})
--- private_dot_config/nvim/lua/arthur/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/arthur/plugins/treesitter.lua
@@ -1,52 +1,63 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  
-  config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
 
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "json",
-        "yaml",
-        "markdown",
-        "markdown_inline",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "query",
-        "vimdoc",
-        "c",
-        "python",
-        "terraform",
-        "hcl",
-        "go",
-        "sql",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    })
-  end,
+	config = function()
+		-- import nvim-treesitter plugin
+		local treesitter = require("nvim-treesitter.configs")
+
+		-- configure treesitter
+		treesitter.setup({
+			-- enable syntax highlighting
+			highlight = {
+				enable = true,
+			},
+			-- enable indentation
+			indent = { enable = true },
+			-- enable autotagging (w/ nvim-ts-autotag plugin)
+			autotag = {
+				enable = true,
+			},
+			-- ensure these language parsers are installed
+			ensure_installed = {
+				"json",
+				"yaml",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"lua",
+				"vim",
+				"dockerfile",
+				"gitignore",
+				"query",
+				"vimdoc",
+				"c",
+				"python",
+				"terraform",
+				"hcl",
+				"go",
+				"sql",
+			},
+			-- Automatically install missing parsers when entering buffer
+			auto_install = true,
+
+			-- Set to true if you want to sync installations
+			sync_install = false,
+
+			-- List of parsers to ignore installing (for example, large parsers)
+			ignore_install = {},
+
+			-- Incremental selection setup
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+	end,
 }
