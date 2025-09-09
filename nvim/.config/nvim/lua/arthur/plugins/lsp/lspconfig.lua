5b3ded54314ade1abb88e4625c51c939d7bfe7d8
Arthur Cesarino <contatocesarino@gmail.com>
2025-09-09 17:41:57 +0200

    fixed bugs from octo and lspconfig

--- nvim/.config/nvim/lua/arthur/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/arthur/plugins/lsp/lspconfig.lua
@@ -5,131 +5,138 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 		{ "folke/neodev.nvim", opts = {} },
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
 	},
+
 	config = function()
-		-- import lspconfig plugin
+		-- Core requires
 		local lspconfig = require("lspconfig")
-
-		-- import mason_lspconfig plugin
-		local mason_lspconfig = require("mason-lspconfig")
-
-		-- import cmp-nvim-lsp plugin
+		local util = require("lspconfig.util")
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-		local keymap = vim.keymap -- for conciseness
+		-- Mason bootstrap
+		require("mason").setup()
+		local mason_lspconfig = require("mason-lspconfig")
 
+		-- Capabilities for nvim-cmp completion
+		local capabilities = cmp_nvim_lsp.default_capabilities()
+
+		-- Keymaps when LSP attaches
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 			callback = function(ev)
+				local km = vim.keymap
 				local opts = { buffer = ev.buf, silent = true }
 
-				-- Keymaps for LSP features
 				opts.desc = "Show LSP references"
-				keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+				km.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
 
 				opts.desc = "Go to declaration"
-				keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+				km.set("n", "gD", vim.lsp.buf.declaration, opts)
 
 				opts.desc = "Show LSP definitions"
-				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+				km.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
 
 				opts.desc = "Show LSP implementations"
-				keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+				km.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
 
 				opts.desc = "Show LSP type definitions"
-				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+				km.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 
-				opts.desc = "See available code actions"
-				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+				opts.desc = "Code action"
+				km.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 
-				opts.desc = "Smart rename"
-				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				opts.desc = "Rename"
+				km.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
-				opts.desc = "Show buffer diagnostics"
-				keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+				opts.desc = "Buffer diagnostics"
+				km.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
 
-				opts.desc = "Show line diagnostics"
-				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+				opts.desc = "Line diagnostics"
+				km.set("n", "<leader>d", vim.diagnostic.open_float, opts)
 
-				opts.desc = "Go to previous diagnostic"
-				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+				opts.desc = "Prev diagnostic"
+				km.set("n", "[d", vim.diagnostic.goto_prev, opts)
 
-				opts.desc = "Go to next diagnostic"
-				keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+				opts.desc = "Next diagnostic"
+				km.set("n", "]d", vim.diagnostic.goto_next, opts)
 
-				opts.desc = "Show documentation for what is under cursor"
-				keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				opts.desc = "Hover"
+				km.set("n", "K", vim.lsp.buf.hover, opts)
 
 				opts.desc = "Restart LSP"
-				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+				km.set("n", "<leader>rs", ":LspRestart<CR>", opts)
 			end,
 		})
 
-		-- used to enable autocompletion (assign to every lsp server config)
-		local capabilities = cmp_nvim_lsp.default_capabilities()
-
-		-- Change the Diagnostic symbols in the sign column (gutter)
+		-- Diagnostic signs
 		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
 
-		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
+		-- Mason-LSPConfig with handlers (modern API)
+		mason_lspconfig.setup({
+			-- Optional: pre-install some servers you use often
+			ensure_installed = { "lua_ls", "terraformls", "tflint", "clangd" },
+			handlers = {
+				-- Default handler (applies to every server)
+				function(server_name)
+					lspconfig[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
 
-			-- Special handler for lua_ls
-			["lua_ls"] = function()
-				lspconfig.lua_ls.setup({
-					capabilities = capabilities,
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-							completion = {
-								callSnippet = "Replace",
+				-- lua_ls (Neovim Lua)
+				["lua_ls"] = function()
+					lspconfig.lua_ls.setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								diagnostics = { globals = { "vim" } },
+								completion = { callSnippet = "Replace" },
+								workspace = {
+									checkThirdParty = false,
+								},
 							},
 						},
-					},
-				})
-			end,
+					})
+				end,
 
-			-- Special handler for terraformls (Terraform Language Server)
-			["terraformls"] = function()
-				lspconfig.terraformls.setup({
-					capabilities = capabilities,
-					filetypes = { "terraform", "hcl" }, -- Ensure the correct filetypes are handled
-					root_dir = lspconfig.util.root_pattern(".terraform", ".git", "main.tf"), -- Recognize the Terraform project root
-				})
-			end,
+				-- Terraform Language Server
+				["terraformls"] = function()
+					lspconfig.terraformls.setup({
+						capabilities = capabilities,
+						filetypes = { "terraform", "hcl" },
+						root_dir = util.root_pattern(".terraform", ".git", "main.tf"),
+					})
+				end,
 
-			-- Special handler for tflint (Linter for Terraform)
-			["tflint"] = function()
-				lspconfig.tflint.setup({
-					capabilities = capabilities,
-				})
-			end,
-			-- ✅ Special handler for clangd (C++ LSP)
-			["clangd"] = function()
-				lspconfig.clangd.setup({
-					capabilities = capabilities,
-					cmd = {
-						"clangd",
-						"--background-index",
-						"--clang-tidy",
-						"--completion-style=detailed",
-						"--cross-file-rename",
-					},
-					filetypes = { "c", "cpp", "objc", "objcpp" },
-					root_dir = lspconfig.util.root_pattern(".clangd", ".git", "compile_commands.json"),
-				})
-			end,
+				-- TFLint
+				["tflint"] = function()
+					lspconfig.tflint.setup({
+						capabilities = capabilities,
+					})
+				end,
+
+				-- clangd (C/C++)
+				["clangd"] = function()
+					lspconfig.clangd.setup({
+						capabilities = capabilities,
+						cmd = {
+							"clangd",
+							"--background-index",
+							"--clang-tidy",
+							"--completion-style=detailed",
+							"--cross-file-rename",
+						},
+						filetypes = { "c", "cpp", "objc", "objcpp" },
+						root_dir = util.root_pattern(".clangd", ".git", "compile_commands.json"),
+					})
+				end,
+			},
 		})
 	end,
 }
